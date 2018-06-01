f6f67a6b5c8c2f6d0f299965a5ce7ae1b01cc776
Samuel Peng <samuelpeng1@gmail.com>
2018-06-01 11:16:39 +0800

    Attempt 7

--- tryCamera.py
+++ tryCamera.py
@@ -20,10 +20,12 @@ class Camera():
         except  TypeError or ValueError:
             raise
 
-    def rotateImage(image, angle):
-        image_center = tuple(np.array(image.shape[1::-1]) / 2)
-        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-        result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    def rotateImage(imagee):
+        #image_center = tuple(np.array(image.shape[1::-1]) / 2)
+        rows, cols = image.shape
+        rot_mat = cv2.getRotationMatrix2D(image_center, 90, 1.0)
+        #result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+        result = cv2.warpAffine(image, rot_mat,(cols, rows))
         return result
 
     def startPreview(self):
@@ -32,7 +34,7 @@ class Camera():
         else:
             rval = false
         while rval:
-            cv2.imshow("Preview", self.rotateImage(frame, 90))
+            cv2.imshow("Preview", self.rotateImage(frame))
             rval, frame = self.vc.read()
             key = cv2.waitKey(20)
             if key == 27:
