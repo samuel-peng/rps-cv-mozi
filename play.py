c945c8e8cc02f31adb838b544bbc43747f129b21
Samuel Peng <samuelpeng1@gmail.com>
2018-06-01 20:41:24 +0800

    normal update

--- play.py
+++ play.py
@@ -1,28 +1,5 @@
 # play.py
 # Source: https://github.com/DrGFreeman/rps-cv
-#
-# MIT License
-#
-# Copyright (c) 2017 Julien de la Bruere-Terreault <drgfreeman@tuta.io>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 # This file is the main program to run to play the Rock-Paper-Scissors game.
 # Game output is made through the terminal and OpenCV window (no GUI).
 
@@ -42,7 +19,7 @@ def saveImage(img, gesture, notify=False):
     # Define image path and filename
     folder = rps.imgPathsRaw[gesture]
     name = rps.gestureTxt[gesture] + '-' + time.strftime('%Y%m%d-%H%M%S')
-    extension = '.png'
+    Uxtension = '.png'
 
     if notify:
         print('Saving {}'.format(folder + name + extension))
